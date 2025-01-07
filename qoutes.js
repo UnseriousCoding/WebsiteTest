698fc0363b8fa0c23badb44d466a058f3aff4e27
UnseriousCoding <ing.mel.lar@gmail.com>
2025-01-07 22:11:18 +0100

    Create qoutes.js

---
+++ qoutes.js
@@ -0,0 +1,8 @@
+// Array med sitater
+const quotes = [
+    "Livet er det som skjer mens du planlegger noe annet. – John Lennon",
+    "Det er aldri for sent å bli den du kunne vært. – George Eliot",
+    "Lykke er ikke noe ferdiglaget. Det kommer fra dine egne handlinger. – Dalai Lama",
+    "Handling er grunnlaget for all suksess. – Pablo Picasso",
+    "Du savner 100% av sjansene du ikke tar. – Wayne Gretzky"
+];
